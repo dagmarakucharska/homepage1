e7e23a3d92da220322afc36a4f80fecccc1ac0d9
dagmara <carribean09@gmail.com>
2019-07-03 22:11:37 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log ('siema');
+const name = 'Dagmara';
+const age = 27;
+console.log(name);
+console.log(age);
+
+console.log (`Nazywam się ${name} i mam ${age} lat`);
+
+alert('Witam Cię na mojej stronie!');
