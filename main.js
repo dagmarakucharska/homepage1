dd1202c97a1931a2addcffd381c83e1bab1c0d8d
dagmara <carribean09@gmail.com>
2019-07-15 14:07:25 +0200

    play with JS

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ console.log (`Nazywam się ${name} i mam ${age} lat`);
 
 alert('Witam Cię na mojej stronie!');
 
-const navigation = document.querySelector('.navigation');
+const about = document.querySelector('.about__paragraph--js');
 
-console.log(navigation);
+console.log(about.innerHTML);
+
+about.innerHTML = 'Pozdrawiam'
+
+const paragraph = document.querySelectorAll('p');
+
+console.log(paragraph);
+console.log(paragraph[1]);
