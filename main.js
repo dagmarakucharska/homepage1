6baba37ad73a66fbd2ed608781033aab0557a1d0
dagmara <carribean09@gmail.com>
2019-08-10 10:57:36 +0200

    JavaScript if else and else if/switch/functions(and fat arrow) in JavaScript

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log ('siema');
 const name = 'Dagmara';
-const age = 27;
+const age = 20;
+let Dagmara = null;
 console.log(name);
 console.log(age);
 
@@ -29,3 +30,62 @@ const paragraph = document.querySelector('.about__paragraph--js')
 console.log(paragraph.innerHTML)
 
 paragraph.innerHTML = 'I tak zamienia się tekst w JavaScripcie!';
+
+/* Jak używać instrukcji if(35-37, 39-40), else(43-45), else if(41-42), switch(47-60) oraz &&(41), ||, zapis skrócony if(62-63*/
+if ('JavaScript' != 'Java') {
+    console.log('to prawda!');
+}
+
+if (age < 20) {
+console.log('masz mniej niż 20 lat');
+} else if ((age >= 20) && (age <= 30)) {
+console.log('masz więcej niż 20 lat, ale mniej niż 30');
+} else {
+console.log('masz więcej niż 31 lat');
+}
+
+switch (age) {
+
+    case 20:
+        console.log('masz 20 lat');
+    break;
+
+    case 30:
+        console.log('masz 30 lat');
+    break;
+
+    default:
+        console.log(`masz ${age} lat`);
+    break;
+}
+
+const amIOld = (age > 70) ? 'yes' : 'no';
+console.log(amIOld);
+
+/* Funkcja w JavaScrypcie, jak ją wywołać(72-76?*/
+function calculate(x) {
+    x = x + 3;
+    console.log(`tradycyjnie: ${x}`);
+    return x*7
+}
+
+calculate(1);
+
+console.log(calculate(2));
+const myCalculation = calculate(4);
+console.log(myCalculation);
+
+/* Fat arrow functions*/
+
+const calculateFat = (x) => {
+    x = x + 3;
+    console.log(`fat: ${x}`);
+    return x*7
+}
+
+console.log(calculateFat(2));
+
+/* Skrócona forma fat arrow functions*/
+
+const calculateFatCut = x => (x+3)*7;
+console.log(calculateFatCut(2));
